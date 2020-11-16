448dd803ec9a4390ccd64b5ca8c0b4041dbad583
andy8052 <andychorlian@gmail.com>
2020-11-16 17:44:42 -0500

    initial commit

--- src/CompHevmIssue.sol
+++ src/CompHevmIssue.sol
@@ -1,4 +1,34 @@
 pragma solidity ^0.6.7;
 
+interface IWETH {
+    function deposit() external payable;
+
+    function withdraw(uint256 wad) external;
+}
+
+interface CEth {
+    function mint() external payable;
+
+    function exchangeRateCurrent() external returns (uint256);
+
+    function supplyRatePerBlock() external returns (uint256);
+
+    function redeem(uint) external returns (uint);
+
+    function redeemUnderlying(uint) external returns (uint);
+}
+
 contract CompHevmIssue {
+    function supplyEthToCompound()
+        public
+    {
+        IWETH(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2).withdraw(1 ether);
+        // Create a reference to the corresponding cToken contract
+        CEth cToken = CEth(0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5);
+
+        cToken.mint.value(1 ether)();
+    }
+        
+    // This is needed to receive ETH when calling `redeemCEth`
+    receive() external payable {}
 }
--- src/CompHevmIssue.t.sol
+++ src/CompHevmIssue.t.sol
@@ -4,18 +4,36 @@ import "ds-test/test.sol";
 
 import "./CompHevmIssue.sol";
 
+interface Hevm {
+    function warp(uint256) external;
+
+    function store(
+        address,
+        bytes32,
+        bytes32
+    ) external;
+}
+
 contract CompHevmIssueTest is DSTest {
     CompHevmIssue issue;
+    Hevm public hevm;
+    IWETH public weth;
 
     function setUp() public {
         issue = new CompHevmIssue();
+
+        hevm = Hevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+
+        weth = IWETH(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+
+        hevm.store(
+            address(weth),
+            keccak256(abi.encode(address(issue), uint256(3))), // Mint us 1000 WETH
+            bytes32(uint256(1000 ether))
+        );
     }
 
-    function testFail_basic_sanity() public {
-        assertTrue(false);
-    }
-
-    function test_basic_sanity() public {
-        assertTrue(true);
+    function test_supply() public {
+        issue.supplyEthToCompound();
     }
 }
